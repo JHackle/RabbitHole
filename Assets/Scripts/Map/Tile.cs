78b4fcbb62f6d5c1af099177555be305f561a165
Hackle <reinke.tobias@gmail.com>
2018-09-28 16:10:44 +0200

    added capability to keep a unit and a building to tile

--- Assets/Scripts/Map/Tile.cs
+++ Assets/Scripts/Map/Tile.cs
@@ -4,5 +4,37 @@
 
     public class Tile : SelectableUnit
     {
+        private IMovable unit;
+        private ISelectable building;
+
+        public bool HasUnit()
+        {
+            return unit != null;
+        }
+
+        public bool HasBuilding()
+        {
+            return building != null;
+        }
+
+        public void SetBuilding(ISelectable building)
+        {
+            this.building = building;
+        }
+
+        public void SetUnit(IMovable unit)
+        {
+            this.unit = unit;
+        }
+
+        public void RemoveBuilding()
+        {
+            building = null;
+        }
+
+        public void RemoveUnit()
+        {
+            unit = null;
+        }
     }
 }
